acb70f68d13e235a8575412cbd62f232c5ef33dc
Lazy_V <954294627@qq.com>
2023-02-07 01:56:26 +0800

    project-structure: Add MaterialUI.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,19 @@ import {
   Spacer
 } from "@nextui-org/react";
 
+import {
+  Alert,
+  AlertTitle
+} from "@mui/material";
+
+import {
+  Home
+} from "@mui/icons-material";
+
 function App() {
 
   function start() {
-    
+
   }
 
   return (
@@ -18,12 +27,18 @@ function App() {
       direction="column"
       justify="center"
       alignItems="center"
-      style={{ marginTop: 90 }}>
+      style={{ marginTop: 30 }}>
+      <Alert severity="info" icon={<Home fontSize="inherit" />}>
+        <AlertTitle>Tips</AlertTitle>
+        The app is still developing !
+      </Alert>
+      <Spacer y={2} />
       <Image src="/logo.png" width={150} height={150} />
       <Spacer y={2} />
       <Text h1>Welcome to Mail Helper Next!</Text>
       <Spacer y={2} />
       <Button onPress={start}>Start</Button>
+
     </Container>
   );
 }
